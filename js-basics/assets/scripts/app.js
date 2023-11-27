9bd50d9ccdc0bc10fce71023a8f7e1a57772b04e
Manoj M <manumadhu1425@gmail.com>
2023-11-27 13:12:20 +0530

    arrays and map() function

--- js-basics/assets/scripts/app.js
+++ js-basics/assets/scripts/app.js
@@ -1,10 +1,26 @@
 // import { apiKey } from "./util.js";
 
-import apiKey from "./util.js";
-import { key } from "./util.js";
-import * as util from "./util.js";
+// import apiKey from "./util.js";
+// import { key } from "./util.js";
+// import * as util from "./util.js";
 
-console.log(apiKey);
-console.log(key);
-console.log(util.default);
-console.log(util.key);
+// console.log(apiKey);
+// console.log(key);
+// console.log(util.default);
+// console.log(util.key);
+
+const hobbies = ["Sports", "Reading", "Singing", "Playing"];
+
+hobbies.push("Stoty telling");
+hobbies.unshift("Sports");
+hobbies.forEach((hobby) => {
+  console.log(hobby);
+});
+let index = hobbies.findIndex((item) => item === "Sports");
+console.log(index);
+const edidtedHobbies = hobbies.map((item) => item + "!");
+console.log(edidtedHobbies);
+const edidtedHobbiesWithObjects = hobbies.map((item) => ({
+  text: item,
+}));
+console.log(edidtedHobbiesWithObjects);
