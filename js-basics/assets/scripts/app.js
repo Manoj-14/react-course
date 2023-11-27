44052f1d61b2364d2348f1c18449934fe0077639
Manoj M <manumadhu1425@gmail.com>
2023-11-27 13:17:43 +0530

    destructuring

--- js-basics/assets/scripts/app.js
+++ js-basics/assets/scripts/app.js
@@ -9,18 +9,28 @@
 // console.log(util.default);
 // console.log(util.key);
 
-const hobbies = ["Sports", "Reading", "Singing", "Playing"];
+// const hobbies = ["Sports", "Reading", "Singing", "Playing"];
 
-hobbies.push("Stoty telling");
-hobbies.unshift("Sports");
-hobbies.forEach((hobby) => {
-  console.log(hobby);
-});
-let index = hobbies.findIndex((item) => item === "Sports");
-console.log(index);
-const edidtedHobbies = hobbies.map((item) => item + "!");
-console.log(edidtedHobbies);
-const edidtedHobbiesWithObjects = hobbies.map((item) => ({
-  text: item,
-}));
-console.log(edidtedHobbiesWithObjects);
+// hobbies.push("Stoty telling");
+// hobbies.unshift("Sports");
+// hobbies.forEach((hobby) => {
+//   console.log(hobby);
+// });
+// let index = hobbies.findIndex((item) => item === "Sports");
+// console.log(index);
+// const edidtedHobbies = hobbies.map((item) => item + "!");
+// console.log(edidtedHobbies);
+// const edidtedHobbiesWithObjects = hobbies.map((item) => ({
+//   text: item,
+// }));
+// console.log(edidtedHobbiesWithObjects);
+
+const [firstname, lastname] = ["Manoj", "M"];
+console.log(firstname);
+console.log(lastname);
+
+const { name, age } = { name: "Manoj", age: 21 };
+console.log(name, age);
+
+const { name: var_name, age1 } = { name: "Manoj", age: 21 };
+console.log(var_name, age1);
