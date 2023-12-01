e62891b73c6df42e0a876e9304dde0212adb976a
Manoj M <manumadhu1425@gmail.com>
2023-12-01 10:40:38 +0530

    Edit and saveing player name

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  let playername = <span className="player-name">{name}</span>;
+
+  function handleClick(setEditing) {
+    setIsEditing(setEditing);
+  }
+
+  if (isEditing) {
+    playername = <input type="name" required />;
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playername}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => handleClick(!isEditing)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
     </li>
   );
 }
